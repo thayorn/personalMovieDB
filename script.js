eb0e3ba3df4208b8213ae47ada1457a5f5ea6c94
Val <gugrant@mail.ru>
2021-05-08 10:32:49 +0300

    practice with circles

--- script.js
+++ script.js
@@ -10,12 +10,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его', '');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = +prompt('На сколько оцените его', '');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('all right');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+let i = 0;
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = +prompt('На сколько оцените его', '');
+          personalMovieDB.movies[a] = b;
+          i++;
+          console.log('all right');
+        }
 
-      console.log(personalMovieDB);
+
+// if (personalMovieDB.count <= 10) {
+//     alert ('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count <= 30) {
+//     alert ('Вы классический зритель');
+// } else if (personalMovieDB.count > 30) {
+//     alert ('Вы киноман');
+// } else {
+//     alert ('Произошла ошибка');
+// }
